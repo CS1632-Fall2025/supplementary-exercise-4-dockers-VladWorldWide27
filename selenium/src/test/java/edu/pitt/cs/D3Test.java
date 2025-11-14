454370fca3a617fec98ce3e2024fddb0e5f58c93
Vladimir Deianov <146010310+VladWorldWide27@users.noreply.github.com>
2025-11-14 05:06:06 -0500

    Update D3Test.java

--- selenium/src/test/java/edu/pitt/cs/D3Test.java
+++ selenium/src/test/java/edu/pitt/cs/D3Test.java
@@ -30,6 +30,9 @@ public class D3Test {
   
   @Before
   public void setUp() {
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+    driver = new ChromeDriver(options);
     driver = new ChromeDriver();
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
